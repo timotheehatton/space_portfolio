e433689dc60878d1db1384b60b26e70cddc77782
timotheehatton <timothee.hatton@gmail.com>
2017-02-22 20:00:18 +0100

    [update] all structurs

--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -1,1 +1,22 @@
+var header = document.querySelector('.header');
 
+function cover()
+{
+  var height = window.innerHeight;
+  height = height + height * 0.2;
+  header.style.height = height + 'px';
+}
+cover();
+window.addEventListener('resize', function(event){
+  cover();
+});
+
+// window.addEventListener( 'scroll', function()
+// {
+//   if (body.scrollTop < 400) {
+//     header_center.style.opacity = 1 - body.scrollTop / 200;
+//     header.style.backgroundPosition = '50% ' + '-' + (body.scrollTop / 2) + 'px';
+//   }
+// });
+//
+// //requestAnimationFrame(fade);

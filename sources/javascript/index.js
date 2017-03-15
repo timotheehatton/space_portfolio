844e1e5e74994bf2b5367dd91d707882a80ae1b1
timotheehatton <timothee.hatton@gmail.com>
2017-03-15 18:22:53 +0100

    :fire: reboot project

--- sources/javascript/index.js
+++ sources/javascript/index.js
@@ -1,22 +1,53 @@
-var header = document.querySelector('.header');
+var body          = document.querySelector('body'),
+    header        = body.querySelector('.header'),
+    title         = header.querySelector('.header--parallax'),
+    slider        = body.querySelector('.portfolio--content--project--slider'),
+    btn_prev      = body.querySelector('.portfolio--content--previous'),
+    btn_next      = body.querySelector('.portfolio--content--next');
 
+// header cover
 function cover()
 {
   var height = window.innerHeight;
-  height = height + height * 0.2;
+  var width  = window.innerWidth;
+  if (width <= 1180) {
+    height = height;
+  }
+  else {
+    height = height + height * 0.2;
+  }
   header.style.height = height + 'px';
 }
 cover();
-window.addEventListener('resize', function(event){
+window.addEventListener('resize', function()
+{
   cover();
 });
 
+// parallax
+function paralax()
+{
+  title.style.transform = "translateY(-" + body.scrollTop * 0.4 + "px)";
+}
+
 // window.addEventListener( 'scroll', function()
 // {
-//   if (body.scrollTop < 400) {
-//     header_center.style.opacity = 1 - body.scrollTop / 200;
-//     header.style.backgroundPosition = '50% ' + '-' + (body.scrollTop / 2) + 'px';
-//   }
+//   window.requestAnimationFrame(paralax);
 // });
-//
-// //requestAnimationFrame(fade);
+
+// slider
+var i = 0;
+btn_next.addEventListener('click', function(event)
+{
+  i++;
+  if (i == 3){ i = 0; }
+  slider.style.transform = "translateX(-" + i * 100 + "%)";
+  event.preventDefault();
+});
+btn_prev.addEventListener('click', function(event)
+{
+  i--;
+  if (i == -1){ i = 2; }
+  slider.style.transform = "translateX(-" + i * 100 + "%)";
+  event.preventDefault();
+});
